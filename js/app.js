06877581087607d6b289381933ea84a9829a46a1
Joram Livengood <joram.livengood@gmail.com>
2018-03-20 15:46:35 -0500

    working my way down index.html populating divs

---
+++ js/app.js
@@ -0,0 +1,19 @@
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ["March 19-25", "March 26-April 1", "April 2-8", "April 9-15", "April 16-22", "April 23-29", "April 30-May6"],
+        datasets: [{
+            label: "",
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45],
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
